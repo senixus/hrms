11b1904d39a785f48daa72bcab96eb93ca1fcd7d
Nihat <senius.33@gmail.com>
2021-06-03 23:03:47 +0300

    Cloudinary api moved to application properties

--- src/main/java/com/hrms/hrms/config/CloudinaryConfig.java
+++ src/main/java/com/hrms/hrms/config/CloudinaryConfig.java
@@ -4,18 +4,25 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.hrms.hrms.core.utilities.cloudinary.CloudinaryManager;
 import com.hrms.hrms.core.utilities.cloudinary.CloudinaryService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cld.access-key}")
+    String apiKey;
+
+    @Value("${cld.secret-key}")
+    String apiSecretKey;
+
     @Bean
     public Cloudinary cloudinaryUser(){
         return new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", "humanrms",
-                "api_key", "115357398659851",
-                "api_secret", "pzK77NN5pZurtYmm01A2n7aen5g"));
+                "api_key", apiKey,
+                "api_secret", apiSecretKey));
     }
 
     @Bean
--- src/main/java/com/hrms/hrms/core/utilities/cloudinary/CloudinaryManager.java
+++ src/main/java/com/hrms/hrms/core/utilities/cloudinary/CloudinaryManager.java
@@ -6,11 +6,13 @@ import com.hrms.hrms.core.utilities.results.DataResult;
 import com.hrms.hrms.core.utilities.results.ErrorDataResult;
 import com.hrms.hrms.core.utilities.results.SuccessDataResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
 
+@Service
 public class CloudinaryManager implements CloudinaryService{
 
     private Cloudinary cloudinary;
@@ -28,7 +30,7 @@ public class CloudinaryManager implements CloudinaryService{
 
             return new SuccessDataResult<Map>(cloudinaryUploader);
         } catch (IOException e) {
-
+                e.printStackTrace();
         }
         return new ErrorDataResult<Map>();
     }
